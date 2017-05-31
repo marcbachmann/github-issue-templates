f4b6884e2909814fdf15ae91055aace90b7721b5
Marc Bachmann <marc.brookman@gmail.com>
2017-05-31 02:40:46 +0200

    Dynamically request permission to custom domains

---
+++ extension/dist/background.js
@@ -0,0 +1,43 @@
+/* globals chrome */
+
+// https://developer.chrome.com/extensions/declarativeContent#type-PageStateMatcher
+chrome.runtime.onInstalled.addListener(updateContentInjector)
+chrome.storage.onChanged.addListener(updateContentInjector)
+
+function updateContentInjector () {
+  getDomains(function (err, domains) {
+    if (err) throw err
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules(domains.map(toRule))
+    })
+  })
+}
+
+function toRule (domain) {
+  return {
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: domain.name, schemes: ['https'] },
+        css: ['a.btn[href$="/issues/new"]']
+      })
+    ],
+    actions: [
+      new chrome.declarativeContent.RequestContentScript({
+        js: ['content.js']
+      })
+    ]
+  }
+}
+
+function filterDomains (items) {
+  return (key) => /^domain:/.test(key) ? items[key] : undefined
+}
+
+function getDomains (cb) {
+  chrome.storage.sync.get(null, function (items) {
+    const domains = Object.keys(items).map(filterDomains(items)).filter(Boolean)
+    if (!domains.find(({name}) => name === 'github.com')) domains.push({name: 'github.com'})
+    console.log(domains)
+    cb(null, domains)
+  })
+}
